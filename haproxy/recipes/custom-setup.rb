4dc181e1f76682dd2462a9c418054a2c1341cac3
Alexandre Salaun <asalaun@siliconsalad.com>
2015-05-04 21:33:17 +0200

    Update gitignore

--- haproxy/recipes/custom-setup.rb
+++ haproxy/recipes/custom-setup.rb
@@ -10,7 +10,7 @@ node[:deploy].each do |app_name, deploy|
       echo deb http://archive.ubuntu.com/ubuntu trusty-backports main universe | \
       sudo tee /etc/apt/sources.list.d/backports.list
       apt-get update -y
-      apt-get install -y haproxy -t trusty-backports
+      apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y haproxy -t trusty-backports
     EOH
   end
 end
