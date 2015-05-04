5e52f2dc42390e2bcdb2a2019b0389d34a806beb
Alexandre Salaun <asalaun@siliconsalad.com>
2015-05-04 17:58:32 +0200

    Add haproxy upgrade recipe

---
+++ haproxy/recipes/custom-setup.rb
@@ -0,0 +1,16 @@
+node[:deploy].each do |app_name, deploy|
+  Chef::Log.info("Upgrade HAProxy")
+
+
+  script "upgrade_haproxy" do
+    interpreter "bash"
+    user "root"
+    cwd "/home/ubuntu/"
+    code <<-EOH
+      echo deb http://archive.ubuntu.com/ubuntu trusty-backports main universe | \
+      sudo tee /etc/apt/sources.list.d/backports.list
+      apt-get update
+      apt-get install haproxy -t trusty-backports
+    EOH
+  end
+end
