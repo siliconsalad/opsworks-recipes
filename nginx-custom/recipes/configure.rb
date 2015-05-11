d516453cd8118c11541f40c481c8d9854ff2fb83
Alexandre Salaun <asalaun@siliconsalad.com>
2015-05-11 09:14:06 +0200

    Add conf for prod haproxy env

--- nginx-custom/recipes/configure.rb
+++ nginx-custom/recipes/configure.rb
@@ -18,16 +18,6 @@ node[:deploy].each do |app_name, deploy|
       )
     end
 
-    script "add_htpasswd" do
-      interpreter "bash"
-      user "root"
-      cwd "/"
-      code <<-EOH
-      touch /etc/nginx/conf.d/htpasswd
-      printf "#{htaccess_username}:$(openssl passwd -crypt #{htaccess_password})\n\" | tee /etc/nginx/conf.d/htpasswd
-      EOH
-    end
-
     service "nginx" do
       action :reload
     end
