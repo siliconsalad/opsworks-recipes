75e8c97d82340300133d87229ba8ee3bd549cf79
Alexandre Salaun <asalaun@siliconsalad.com>
2015-06-24 18:44:48 +0200

    Add curl

---
+++ curl/recipes/install.rb
@@ -0,0 +1,14 @@
+Chef::Log.info("Configure Nginx")
+
+node[:deploy].each do |app_name, deploy|
+  if defined?(deploy[:application_type]) && deploy[:application_type] == 'rails'
+    script "add_htpasswd" do
+      interpreter "bash"
+      user "root"
+      cwd "/"
+      code <<-EOH
+      apt-get install -y libcurl4-dev
+      EOH
+    end
+  end
+end
