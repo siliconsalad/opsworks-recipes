562f956d5e785a93323a02778c0c1a86391a180f
Alexandre Salaun <asalaun@siliconsalad.com>
2015-06-26 17:12:32 +0200

    Update dj recipe

--- opsworks_delayed_job/recipes/deploy.rb
+++ opsworks_delayed_job/recipes/deploy.rb
@@ -1,6 +1,6 @@
 # Adapted from deploy::rails: https://github.com/aws/opsworks-cookbooks/blob/master/deploy/recipes/rails.rb
 
-include_recipe 'deploy'
+# include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
 
@@ -9,18 +9,25 @@ node[:deploy].each do |application, deploy|
     next
   end
 
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-    path deploy[:deploy_to]
-  end
+  # opsworks_deploy_dir do
+  #   user deploy[:user]
+  #   group deploy[:group]
+  #   path deploy[:deploy_to]
+  # end
 
   include_recipe "opsworks_delayed_job::setup"
 
-  node.set[:opsworks][:rails_stack][:restart_command] = node[:delayed_job][application][:restart_command]
-
-  opsworks_deploy do
-    deploy_data deploy
-    app application
+  bash "restart-all-delayed_job" do
+    user 'root'
+    code <<CODE
+    monit -g delayed_job restart
+    CODE
   end
+
+  # node.set[:opsworks][:rails_stack][:restart_command] = node[:delayed_job][application][:restart_command]
+
+  # opsworks_deploy do
+  #   deploy_data deploy
+  #   app application
+  # end
 end
