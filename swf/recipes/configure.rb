c9b1480281501ba6a2c3da893e803124f96c2f4f
Benoit Vasseur <bvasseur@siliconsalad.com>
2015-10-12 18:29:03 +0200

    fix PIDFILE arg for rake tasks

--- swf/recipes/configure.rb
+++ swf/recipes/configure.rb
@@ -6,13 +6,13 @@ node[:deploy].each do |app_name, deploy|
 
   execute "launch rake task 'workflow_worker' in background " do
     cwd deploy[:current_path]
-    command "RAILS_ENV=#{deploy[:rails_env]} BACKGROUND=y bundle exec rake swf:workflow_worker"
+    command "RAILS_ENV=#{deploy[:rails_env]} BACKGROUND=y PIDFILE=/srv/www/#{deploy[:application]}/shared/rake-workflow_worker.pid bundle exec rake swf:workflow_worker"
     action :run
   end
 
   execute "launch rake task 'activity_worker' in background " do
     cwd deploy[:current_path]
-    command "RAILS_ENV=#{deploy[:rails_env]} BACKGROUND=y bundle exec rake swf:activity_worker"
+    command "RAILS_ENV=#{deploy[:rails_env]} BACKGROUND=y PIDFILE=/srv/www/#{deploy[:application]}/shared/rake-activity_worker.pid bundle exec rake swf:activity_worker"
     action :run
   end
 
